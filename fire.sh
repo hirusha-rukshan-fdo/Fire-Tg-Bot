a84b8c2f660f874ebff47aed9120f03f4b8fbbef
hirusha-rukshan-fdo <107447421+hirusha-rukshan-fdo@users.noreply.github.com>
2022-08-11 12:20:03 +0530

    Update fire.sh

--- fire.sh
+++ fire.sh
@@ -1,6 +1,6 @@
 export TERM=xterm;
 
-git clone "${REPO_URL:-https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2}" Auto_Filter
+git clone "${REPO_URL:-https://github.com/hirusha-rukshan-fdo/Fire-Tg-Bot}" Auto_Filter
 cd Auto_Filter
-echo "Starting...!";
+echo "Starting...! Fire Bot ❤️";
 python3 -m bot
